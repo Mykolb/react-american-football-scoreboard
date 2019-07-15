c67e5753cec420341a58e556e84141b03f7ee77d
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-07-15 16:23:32 -0400

    mvp met

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
     // const [currentValue, setState] = useState(0);
   const [homeTeamScore, setHomeTeamScore] = useState(0) //setting initial state
-
-
+  
   const [awayTeamScore, setAwayTeamScore] = useState(0)
 
 
@@ -31,6 +30,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
+      {/* {homeTeamScore === 20 || awayTeamScore === 20 ? "Lions crushed the Tigers" : "Tigers won"} */}
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
